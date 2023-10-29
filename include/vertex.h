090875e34ca5c017a630b3e53932b0ef495f3274
seanchen <idig@idigs-mbp-2.mynetworksettings.com>
2023-10-29 16:33:19 -0400

    breploader remove parasolid

---
+++ include/vertex.h
@@ -0,0 +1,37 @@
+#ifndef VERTEX_H_INCLUDED
+#define VERTEX_H_INCLUDED 1
+
+#include <Eigen/Core>
+#include <vector>
+#include <TopoDS_Vertex.hxx>
+#include "types.h"
+
+namespace pspy {
+
+struct Vertex {
+    Eigen::Vector3d position;
+    std::string export_id;
+
+    virtual std::vector<Inference> get_inferences() = 0;
+
+};
+
+struct PSVertex: public Vertex {
+    PSVertex(int id, std::string export_id);
+
+    int _id;
+
+    std::vector<Inference> get_inferences() override;
+};
+
+struct OCCTVertex: public Vertex {
+    OCCTVertex(const TopoDS_Shape& shape);
+
+    TopoDS_Vertex _shape;
+
+    std::vector<Inference> get_inferences() override;
+};
+
+}
+
+#endif // !VERTEX_H_INCLUDED
